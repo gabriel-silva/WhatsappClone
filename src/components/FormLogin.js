b98190e15737c8c0d9e442d10e6b05226fd31013
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-09 15:00:51 -0300

    estilizando tela de login

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -3,25 +3,27 @@ import {
     View,
     Text,
     TextInput,
-    Button
+    Button,
+    StyleSheet
 } from 'react-native';
 
 export default props => (
-    <View>
+    <View style={styles.view}>
 
-        <View>
-            <Text> WhatsApp Clone </Text>
+        <View style={styles.viewTopo}>
+            <Text style={styles.txtWhatsapp}> WhatsApp Clone </Text>
         </View>
 
-        <View>
-            <TextInput placeholder='E-mail' />
-            <TextInput placeholder='Senha' />
-            <Text>Ainda não tem cadastro? cadastre-se</Text>
+        <View style={styles.viewLogin}>
+            <TextInput style={styles.txtLogin} placeholder='E-mail' />
+            <TextInput style={styles.txtLogin} placeholder='Senha' />
+            <Text style={styles.txtCadastro}>Ainda não tem cadastro? cadastre-se</Text>
         </View>
 
-        <View>
+        <View style={styles.viewAcessar}>
             <Button
                 title='Acessar'
+                color='#115E54'
                 onPress={() => false}
             />
         </View>
@@ -29,3 +31,31 @@ export default props => (
     </View>
 
 );
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        padding: 10
+    },
+    viewTopo: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    viewLogin: {
+        flex: 2
+    },
+    viewAcessar: {
+        flex: 2
+    },
+    txtWhatsapp: {
+        fontSize: 25,
+    },
+    txtLogin: {
+        fontSize: 20,
+        height: 45
+    },
+    txtCadastro: {
+        fontSize: 20
+    }
+});
