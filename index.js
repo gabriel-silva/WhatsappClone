37e1c46e057ced52f2a9610abc3742586de53112
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-12 14:59:11 -0300

    adicionando conexão com firebase

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Routes from './src/Routes';
 import reducers from './src/reducers';
+import firebase from 'firebase';
 
-const App = props => (
-  <Provider store={createStore(reducers)}>
-    <Routes />
-  </Provider>
-);
+class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB-vLSUdczz_RgpDxKrs08_cNtDCluGQ6s",
+      authDomain: "whatsapp-clone-64532.firebaseapp.com",
+      databaseURL: "https://whatsapp-clone-64532.firebaseio.com",
+      projectId: "whatsapp-clone-64532",
+      storageBucket: "whatsapp-clone-64532.appspot.com",
+      messagingSenderId: "904762112537"
+    });
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <Routes />
+      </Provider>
+    );
+  }
+}
 
 AppRegistry.registerComponent('WhatsappClone', () => App);
