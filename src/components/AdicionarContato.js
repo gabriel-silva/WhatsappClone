6c36a049ae42adcf62be83df5aadf9493f996835
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-13 14:53:06 -0300

    correções no component AdicionarContato

--- src/components/AdicionarContato.js
+++ src/components/AdicionarContato.js
@@ -2,10 +2,33 @@ import React from 'react';
 import {
     View,
     Text,
+    TextInput,
+    Button
 } from 'react-native';
 
 export default props => (
-    <View>
-        <Text> Adicionar Contatos</Text>
+    <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20
+    }}>
+
+        <View style={{
+            flex: 1,
+            justifyContent: 'center'
+        }}>
+            <TextInput
+                placeholder='E-mail'
+                style={{ fontSize: 20, height: 45 }}
+                onChangeText={() => false} />
+        </View>
+
+        <View>
+            <Button
+                title='Adicionar'
+                color='#115E54'
+                onPress={() => false} />
+        </View>
+
     </View>
 );
